19df967bd09449d12dff93b2d789fae48282176b
Benjamin.M <benjmonte@outlook.fr>
2024-04-01 11:48:38 +0200

    Added basic page Users + navigaiton + adapt welcome page

--- README-local.md
+++ README-local.md
@@ -4,3 +4,10 @@ Processus de lancement du projet en local
 
 - > cd C:\wamp64\www\tierlistplus-bo\
 - > php artisan serve
+
+## TailwindCSS
+
+Processus d'installation, de lancement && compilation Tailwind
+
+- > npm i
+- > php run dev
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/dashboard/users', function () {
+        return view('users');
+    })->name('users');
 });
