5ad533ba2f76de643b177c7e4655b352414b9080
Benjamin.M <benjmonte@outlook.fr>
2024-04-04 20:06:40 +0200

    Added base controller User

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-5 sm:flex">
-                    <x-nav-link href="{{ route('users') }}" :active="request()->routeIs('users')">
+                    <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
                         {{ __('Users') }}
                     </x-nav-link>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
+Route::group(['prefix' => 'dashboard', 'middleware' => [
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
+    'verified',]
+], function(){
+
+    // Home
+    Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::get('/dashboard/tierlists', function () {
+
+    // Users
+    Route::get('/tierlists', function () {
         return view('tierlists');
     })->name('tierlists');
-    Route::get('/dashboard/users', function () {
-        return view('users');
-    })->name('users');
+
+    // Users
+    Route::get('/users', UserController::class .'@index')->name('users.index');
+    Route::get('/user/{id}', UserController::class .'@show')->name('users.show');
+    Route::get('/user/{id}/edit', UserController::class .'@edit')->name('users.edit');
+    Route::put('/user/{id}', UserController::class .'@update')->name('users.update');
+    Route::delete('/user/{id}', UserController::class .'@destroy')->name('users.destroy');
 });
