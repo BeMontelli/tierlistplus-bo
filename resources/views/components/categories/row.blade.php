ae606d9ab2165196a81af20167d4b5acdea70a30
Benjamin.M <benjmonte@outlook.fr>
2024-04-07 17:00:03 +0200

    Fix layout

--- resources/views/components/categories/row.blade.php
+++ resources/views/components/categories/row.blade.php
@@ -1,7 +1,9 @@
 <div class="row mb-4 ease-in duration-100 rounded-[10px] hover:p-2 hover:pl-4 hover:bg-gray-100 dark:hover:bg-gray-600">
     <div class="flex">
         <div class="id flex items-center flex-none w-10 font-medium text-gray-900 dark:text-white">{{$category->id}}</div>
-        <div class="name flex items-center w-5 text-left flex-auto font-medium text-gray-900 dark:text-white">{{$category->title}}</div>
+        <div class="name flex items-center w-5 text-left flex-auto font-medium text-gray-900 dark:text-white">
+            <a class="hover:text-orange-400 transition ease-in" href="{{ route('categories.show', $category->id) }}">{{$category->title}}</a>
+        </div>
         <div class="actions flex flex-none ">
             <a href="{{ route('categories.edit', $category->id) }}" class="mr-2 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Edit</a>
             <form action="{{ route('categories.destroy', $category->id) }}" method="post">
--- resources/views/tierlists/show.blade.php
+++ resources/views/tierlists/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Category') }}
+            {{ __('Tierlist') }}
         </h2>
     </x-slot>
 
@@ -12,12 +12,12 @@
                 <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
 
                     <h1 class="mb-6 text-2xl font-medium text-gray-900 dark:text-white">
-                        {{ __('Category') }} : {{$category->title}}
+                        {{ __('Tierlist') }} : {{$tierlist->title}}
                     </h1>
 
-                    <div class="mb-10">{!! $category->description !!}</div>
+                    <div class="mb-10">{!! $tierlist->description !!}</div>
 
-                    <a href="{{ route('categories.edit', $category->id) }}" class="mr-2 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Edit</a>
+                    <a href="{{ route('tierlists.edit', $tierlist->id) }}" class="mr-2 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Edit</a>
 
                 </div>
 
