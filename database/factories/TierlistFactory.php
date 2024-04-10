8a167396e2615d2db276c6f3f948f4fe2a568f9b
Benjamin.M <benjmonte@outlook.fr>
2024-04-10 18:34:59 +0200

    Added TierlistSeeder.php && TierlistFactory.php

---
+++ database/factories/TierlistFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tierlist>
+ */
+class TierlistFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(2, true),
+            'description' => fake()->paragraph(),
+            'user_id' => User::all()->random()->id,
+        ];
+    }
+}
