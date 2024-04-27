148648ebd54df1c855b49ab7fd412ff84c03b6b6
Benjamin.M <benjmonte@outlook.fr>
2024-04-27 19:05:22 +0200

    Added base api /register && /login

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiUserController;
 
 Route::group(['prefix' => '/v1'], function(){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
+    Route::get('/user', ApiUserController::class .'@user')->name('api.user')->middleware('auth:sanctum');
+    Route::post('/register', ApiUserController::class .'@register')->name('api.user.register');
+    Route::post('/login', ApiUserController::class .'@login')->name('api.user.login');
 });
 
